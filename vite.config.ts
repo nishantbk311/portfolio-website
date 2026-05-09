cd00e8b92db6310e8bbd89a453cae781422e107b
Nishant Bk <125780674+nishantbk311@users.noreply.github.com>
2026-05-09 12:27:09 +0545

    path to /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
-    base: "/portfolio-website/",
+    base: "/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
