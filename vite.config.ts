933cf73791c7bd42fbd405d52ebaa04191500a46
Nishant Bk <125780674+nishantbk311@users.noreply.github.com>
2026-05-09 12:00:29 +0545

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({mode}) => {
   return {
-    base: "/",
+    base: "portfolio-website",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
